8bd6f2debc4b973d67e6c7b764bf4e945d215e2d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-06 07:41:00 +0000

    feat(Elements): add the class as the array state elements for Queue and Stack.

---
+++ src/lib/elements.class.ts
@@ -0,0 +1,9 @@
+import { ArrayState } from "@typescript-package/state";
+/**
+ * @description Array state elements in data structures such as Stack and Queue.
+ * @export
+ * @class Elements
+ * @template Type 
+ * @extends {ArrayState<Type>}
+ */
+export class Elements<Type> extends ArrayState<Type>{};
