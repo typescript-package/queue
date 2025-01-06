b762baad0f94ce486f60f627275262a5ff805744
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-06 08:57:29 +0000

    chore(index): add `Elements`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
+// Class.
+export { Elements } from './elements.class';
+// Abstract.
 export { ProcessingQueue } from './processing-queue.class';
 export { Queue } from './queue.abstract';
 export { Stack } from './stack.abstract';
