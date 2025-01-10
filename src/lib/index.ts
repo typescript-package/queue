71e3b5e6ed3328e92381789898e1671f1b819b66
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-10 10:33:36 +0000

    chore(lib/index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 // Class.
 export { Elements } from './elements.class';
+export { Processing } from './processing.class';
 // Abstract.
 export { ProcessingQueue } from './processing-queue.class';
 export { Queue } from './queue.abstract';
