991e4565261009f93dec5b1a1f979ee34f80970f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-12 16:14:29 +0000

    test(Processing): update.

--- src/test/processing.spec.ts
+++ src/test/processing.spec.ts
@@ -1,18 +1,60 @@
 import { Processing } from "../lib/processing.class";
 
-const processing = new Processing(3);
+let processing = new Processing();
 
-processing.debug().asyncRun(
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  element => console.log(`Processed element: `, element),
-  error => console.log(`Error`, error),
-  'default'
-);
+describe(`Processing`, () => {
+  beforeEach(() => {
+    processing = new Processing()
+      // .debug();
+  });
+  it(`add()`, () => {
+    processing.add(new Promise((resolve, reject) => resolve()));
+    expect(processing.activeCount).toEqual(1);
+    expect(processing.active).toBeTrue();
+  });
+  it(`add(promise, false) + delete() + active + isActive()`, () => {
+    processing.add(new Promise((resolve, reject) => resolve()), false);
+    expect(processing.activeCount).toEqual(1);
+    expect(processing.active).toBeTrue();
 
-processing.unDebug().asyncRun(
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  element => console.log(`Processed element: `, element),
-  error => console.log(`Error`, error),
-  'default'
-);
+    processing.add(new Promise((resolve, reject) => resolve()), false);
+    expect(processing.activeCount).toEqual(2);
+    expect(processing.active).toBeTrue();
+    expect(processing.isActive()).toBeTrue();
+    expect(processing.isActive(true)).toBeTrue();
+    expect(processing.isActive(false)).toBeFalse();
 
+    processing.add(new Promise((resolve, reject) => resolve()), false);
+    expect(processing.activeCount).toEqual(3);
+    expect(processing.active).toBeTrue();
+    expect(processing.isActive()).toBeTrue();
+    expect(processing.isActive(true)).toBeTrue();
+    expect(processing.isActive(false)).toBeFalse();
+
+    processing.delete();
+    expect(processing.activeCount).toEqual(2);
+    expect(processing.active).toBeTrue();
+    expect(processing.isActive()).toBeTrue();
+    expect(processing.isActive(true)).toBeTrue();
+    expect(processing.isActive(false)).toBeFalse();
+
+    processing.delete();
+    expect(processing.activeCount).toEqual(1);
+    expect(processing.active).toBeTrue();
+    expect(processing.isActive()).toBeTrue();
+    expect(processing.isActive(true)).toBeTrue();
+    expect(processing.isActive(false)).toBeFalse();
+
+    processing.delete();
+    expect(processing.activeCount).toEqual(0);
+    expect(processing.active).toBeFalse();
+    expect(processing.isActive()).toBeFalse();
+    expect(processing.isActive(true)).toBeFalse();
+    expect(processing.isActive(false)).toBeTrue();
+
+    processing.complete().finally(() => {
+      expect(true).toBeTrue();
+      console.log(`Processing.activeCount:`, processing.activeCount);
+    });
+  });
+});
