bbe220a7e614aae6a7d383b4f2145ff97a2e4580
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 00:58:52 +0000

    test(Tasks): update the spec.

--- src/test/tasks.spec.ts
+++ src/test/tasks.spec.ts
@@ -1,3 +1,4 @@
+import { Processed } from '../enum/processed.enum';
 import { Tasks } from '../lib/tasks.class';
 
 console.group(`Tasks`);
@@ -6,9 +7,16 @@ let tasks = new Tasks<number>(true, 3);
 
 tasks.debug().asyncRun(
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  element => {},
-  () => {},
-  element => {},
+  element => {
+    console.debug(`Processing, `, element);
+    if (element === 5) {
+      return Processed.Failure;
+    }
+    return Processed.Success;
+  },
+  element => console.debug(`Processed success, `, element),
+  element => console.debug(`Processed failure, `, element),
+  (context, payload, message, type) => {},
   'default'
 ).then(processed => {
   console.log(`----------> Tasks then()`, processed);
