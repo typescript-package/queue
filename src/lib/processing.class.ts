9d0caaa7db534dea1be1ad54319aa57daafe39c1
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-19 01:00:44 +0000

    refactor(Processing): use the console from private getter.

--- src/lib/processing.class.ts
+++ src/lib/processing.class.ts
@@ -48,6 +48,15 @@ export class Processing extends State<Set<Promise<void>>> {
   }
 
   /**
+   * @description Display the console debug on debug state `true`.
+   * @readonly
+   * @type {*}
+   */
+  get #console() {
+    return this.#debug.isTrue() ? console : undefined;
+  }
+
+  /**
    * @description
    * @type {*}
    */
@@ -69,7 +78,7 @@ export class Processing extends State<Set<Promise<void>>> {
    */
   public add(promise: Promise<void>, remove: boolean = true): this {
     super.state.add(promise);
-    this.#consoleDebug("`Promise` added to processing state", { active: this.active, activeCount: this.activeCount });
+    this.#console?.debug("`Promise` added to processing state", { active: this.active, activeCount: this.activeCount });
     remove === true && promise.finally(() => this.delete(promise));
     return this;
   }
@@ -81,12 +90,12 @@ export class Processing extends State<Set<Promise<void>>> {
    * @returns {Promise<void>} 
    */
   public async complete(): Promise<void> {
-    this.#consoleDebug("Invoked `Processing.complete()` to wait for all processes from the state ", { activeCount: this.activeCount })
+    this.#console?.debug("Invoked `Processing.complete()` to wait for all processes from the state ", { activeCount: this.activeCount })
     const promise = Promise.all(super.state);
     await promise;
     if (this.#debug.isTrue()) {
       promise.finally(() => 
-        this.#consoleDebug("`Processing.complete()` finally method invoked.", { activeCount: this.activeCount })
+        this.#console?.debug("`Processing.complete()` finally method invoked.", { activeCount: this.activeCount })
       );
     }
   }
@@ -107,9 +116,9 @@ export class Processing extends State<Set<Promise<void>>> {
    * @returns {this} 
    */
   public delete(promise: Promise<void> = this.first): this {
-    this.#consoleDebug("`activeCount` state before removing the `Promise`", { activeCount: this.activeCount })
+    this.#console?.debug("`activeCount` state before removing the `Promise`", { activeCount: this.activeCount })
     super.state.delete(promise);
-    this.#consoleDebug("`Promise` removed from processing state", { activeCount: this.activeCount });
+    this.#console?.debug("`Promise` removed from processing state", { activeCount: this.activeCount });
     return this;
   }
 
@@ -129,17 +138,6 @@ export class Processing extends State<Set<Promise<void>>> {
    */
   public unDebug(): this {
     this.#debug.false();
-    return this;
-  }
-
-  /**
-   * @description Display the console debug on debug state `true`.
-   * @param {string} message 
-   * @param {?*} [data] 
-   * @returns {this} 
-   */
-  #consoleDebug(message: string, data?: any): this {
-    this.#debug.isTrue() && console.debug(message, data || '');
     return this;
   }
 }
